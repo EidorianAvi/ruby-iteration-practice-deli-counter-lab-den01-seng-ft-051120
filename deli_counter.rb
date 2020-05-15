5314c978414f93f228d6803583f108a56b86f6b9
Rene Avila <reneavila1993@gmail.com>
2020-05-15 03:14:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def take_a_number(array, name)
   array << name
-  position = array.index(name)
-  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  
+  position = array.index(name) + 1
+  puts "Welcome, #{name}. You are number #{position} in line."
 end
+
