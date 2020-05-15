939ae4180949b25bc48f4cb3442fb178abc8b193
Rene Avila <reneavila1993@gmail.com>
2020-05-15 03:11:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,9 @@ def line(array)
   if array.length ==  0 
     puts "The line is currently empty."
   else 
-    output = "The line is currently: "
+    output = "The line is currently:"
     array.each_with_index do |name, index|
-      output += "#{index + 1}. #{name}"
+      output += " #{index + 1}. #{name}"
     end
    puts "#{output}"
   end
